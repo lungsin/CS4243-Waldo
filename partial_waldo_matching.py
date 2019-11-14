62749ec7fa65bb6717c6d7b8372564bd65a718d1
Agus Sentosa Hermawan <agus.sentosa787@gmail.com>
2019-11-14 17:04:04 +0700

    Add more template

--- partial_waldo_matching.py
+++ partial_waldo_matching.py
@@ -16,20 +16,17 @@ def template_matching(image, template, method = cv2.TM_CCOEFF_NORMED, threshold 
     for pt in zip(*loc[::-1]):
         total.append([pt[0], pt[1], pt[0] + w, pt[1] + h])
     
-    return total
+    return np.asarray(total)
         
-def partial_matching_waldo_pyramid(image):
-    template = cv2.imread(f"datasets/Features/partial_waldo.jpg")
+def partial_matching_waldo_pyramid(image, template):
+    template = cv2.imread(f"datasets/Features/{template}.jpg")
     template = resize(template, 128)
     if image.shape[0] < 6000:
         image = resize(image, 6000)
     
     total = []
     for i in pyramid(image, downscale=1.2, min_size=template.shape): 
-        boxes = template_matching(i, template)
-        for box in boxes:
-            for idx in range(len(box)):
-                box[idx] *= image.shape[0] / i.shape[0] 
+        boxes = template_matching(i, template, threshold=0.375) * (image.shape[0] / i.shape[0])
         total.extend(boxes)
     
     total = np.asarray(total)
@@ -42,7 +39,12 @@ if __name__ == "__main__":
     img = cv2.imread(f"datasets/JPEGImages/{sys.argv[1]}.jpg")
     # img = cv2.imread(f"cropped_data/positive/waldo/{sys.argv[1]}.jpg")
 
-    total = partial_matching_waldo_pyramid(img)
+    total = []
+    total.extend(partial_matching_waldo_pyramid(img, "027_0"))
+    total.extend(partial_matching_waldo_pyramid(img, "waldo_face_eye"))
+    total = np.asarray(total)
+    total = non_max_suppression_fast(total, 0)
+
     for j in total:
         img2 = cv2.rectangle(img, (j[0], j[1]), (j[2], j[3]), color=(0, 255, 0), thickness=10)
     display(img)
